241cc46d12a5417de91d7b1c45096a106085af90
Lxtharia <87075045+Lxtharia@users.noreply.github.com>
2022-08-13 12:41:08 +0200

    Disable current pressed button cause styling doesnt work for java 1.8 idk

--- src/pixelsorter/MyButtonGroup.java
+++ src/pixelsorter/MyButtonGroup.java
@@ -3,6 +3,7 @@ package pixelsorter;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MyButtonGroup {
 
@@ -10,20 +11,19 @@ public class MyButtonGroup {
 
     public MyButtonGroup(JButton... bs) {
         buttons = new ArrayList<>();
-        for(JButton b: bs)
-            buttons.add(b);
+        buttons.addAll(Arrays.asList(bs));
     }
     //TODO: change the auto-"tab"-selected button to be always a specific component cause else it's annoying H
     void setSelected(JButton button) {
         for (JButton b : buttons) {
             if (b == button) {
-//                b.setEnabled(false);
+                b.setEnabled(false);
 //                b.setBorder(BorderFactory.createLineBorder(new Color(0x3376A5), 2, true));
                 b.setBackground(new Color(0xB6B6B6));
                 b.setBorderPainted(false);
             }
             else{
-//                b.setEnabled(true);
+                b.setEnabled(true);
                 b.setBackground(new Color(0x9F9F9F));
                 b.setBorderPainted(true);
             }
